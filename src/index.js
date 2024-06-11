cf55cb16d612cc44b0587cfadb28ac1b133de42e
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-06-11 15:16:07 +0430

    getting know functional programming

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+function sayHello() {
+  return "Hello World";
+}
+// assign a fn variable
+let fn = sayHello;
+fn();
+sayHello();
+
+// assign fn as param
+function greet(fnMessage) {
+  console.log(fnMessage);
+}
+
+greet(sayHello);
+
+// return a function inside another fn
+function sayHello2() {
+  return function () {
+    console.log("Hello again");
+  };
+}
+
+let fn2 = sayHello2();
+let message = fn2();
