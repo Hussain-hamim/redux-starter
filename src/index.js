f107b9a81f33c2b81326eed6247f7b5a41324523
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-06-11 16:04:19 +0430

    composing and piping using lodash

--- src/index.js
+++ src/index.js
@@ -1,48 +1,4 @@
-function sayHello() {
-  return "Hello World";
-}
-// assign a fn variable
-let fn = sayHello;
-fn();
-sayHello();
-
-// assign fn as param
-function greet(fnMessage) {
-  console.log(fnMessage);
-}
-
-greet(sayHello);
-
-// return a function inside another fn
-function sayHello2() {
-  return function () {
-    console.log("Hello again");
-  };
-}
-
-let fn2 = sayHello2();
-let message = fn2();
-
-// higher order function: is fn that take fn as argument
-// or return a fn
-function greet2(fn) {
-  console.log(fn());
-}
-
-function sayHello3() {
-  return function () {
-    console.log("Hello again");
-  };
-}
-
-// map is example of HOF cuz it take fn as argument
-let numbers = [1, 2, 3];
-numbers.map((number) => number * 2);
-
-// sitTimeOut is example of HOF cuz it take fn as argument
-setTimeout(() => {
-  console.log("hey");
-}, 1000);
+import { compose, pipe } from "lodash/fp";
 
 ////// function composition:
 
@@ -58,3 +14,9 @@ const toLowerCase = (str) => str.toLowerCase();
 // function composition
 const result = wrapInDiv(trim(input)); // read from right to left
 const result2 = wrapInDiv(toLowerCase(trim(input)));
+
+// using lodash
+const transform = compose(wrapInDiv, toLowerCase, trim); // HO fn, right to left
+const transform2 = pipe(trim, toLowerCase, wrapInDiv); // HO fn, left to right
+transform(input); // hof
+transform2(input); // hof
