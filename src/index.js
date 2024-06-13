de3daf5eca0e3e806d2f1fcda58df927c1f2e68d
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-06-13 10:17:12 +0430

    exercise- implementing bug resolve

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import store from "./store";
-import { bugAdded, bugRemoved } from "./actions";
+import { bugAdded, bugRemoved, bugResolved } from "./actions";
 
 const unsubscribe = store.subscribe(() => {
   console.log("store changed", store.getState());
@@ -9,6 +9,8 @@ console.log(store.getState()); // return the initial state
 
 store.dispatch(bugAdded("bug... 1"));
 console.log(store.getState()); // return the state with a bug added
+
+store.dispatch(bugResolved(1));
 
 unsubscribe();
 
--- src/reducer.js
+++ src/reducer.js
@@ -14,6 +14,14 @@ export default function reducer(state = [], action) {
     ];
   } else if (action.type === actions.BUG_REMOVED) {
     return state.filter((bug) => bug.id !== action.description.id);
+  } else if (action.type === actions.BUG_RESOLVED) {
+    return state.map(
+      (bug) => (bug.id !== action.payload.id ? bug : { ...bug, resolved: true })
+      /** if the id of the bug we rendering, is not equal to the bug
+       * we have resolved then return as it is otherwise return
+       * the a new bug object spreading the previous properties of bug
+       * and add new property resolved: true  */
+    );
   }
 
   return state;
