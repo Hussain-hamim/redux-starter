96ed6d0f3324b61122240dd91d4372d466c2d05b
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-06-13 15:13:13 +0430

    working on a little on redux toolkit RTK

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-// import { createStore } from "redux";
+// import { legacy_createStore as createStore } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 
