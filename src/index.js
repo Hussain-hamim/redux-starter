9c11d15850820b53adaa0dc5aa8fc19811ae207e
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-06-13 15:52:34 +0430

    creating redux from scratch- creating store

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-import store from "./store";
-import { bugAdded, bugRemoved, bugResolved } from "./actions";
+import store from "./customStore";
 
-console.log(store);
+store.getState = 1;
 
-const unsubscribe = store.subscribe(() => {
-  console.log("store changed", store.getState());
-});
-
-console.log(store.getState()); // return the initial state
-
-store.dispatch(bugAdded("bug... 1"));
-console.log(store.getState()); // return the state with a bug added
-
-store.dispatch(bugResolved(1));
-
-unsubscribe();
-
-store.dispatch(bugRemoved(1));
-console.log(store.getState()); // return the state with a bug removed
+console.log(store.getState);
